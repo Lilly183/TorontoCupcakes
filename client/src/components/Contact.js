146b7d1d5eb0e10f11dd30388acaf407c05908bb
Lilly183 <116189687+Lilly183@users.noreply.github.com>
2023-04-24 05:47:08 -0400

    Commit 25
    Removed extra spaces from Contact component (For screenshot purposes)

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,25 +9,21 @@ const Contact = () =>
     return (
         <main>
             <section className="contact spad">
-                <div className="pageContainer container">
-                    
+                <div className="pageContainer container">                    
                     <div className="container pt-2 pb-4">
                         <h2 className="pageHeading text-start text-black">Contact</h2>
                     </div>
-
                     <div className="container">
                         <div className="row">
                             <div className="col-12 text-center"><Map/></div>
                         </div>
                     </div>
-
                     <div className="container">
                         <div className="row my-3">
                             <div className="col"><BusinessHours/></div>
                             <div className="col mx-auto"><ContactInfo/></div>
                         </div>
                     </div>
-
                 </div>
             </section>
         </main>
